bd1cf401596e349c1238f03cdf94855b8aecbac9
FrankRC1 <frankrua181119@gmail.com>
2024-11-28 04:33:42 -0500

    prefijo

--- public/js/prefijo.js
+++ public/js/prefijo.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   // Seleccionamos el input de telefono
   const telefonoInput = document.getElementById('telefono');
   
-  // Agregar un listener para el evento 'input' (cuando el usuario escribe)
+  // Agregar un listener para el evento 'input'
   telefonoInput.addEventListener('input', function() {
       let telefono = telefonoInput.value.trim();
 
